e18217b5211657adc874a28a4fcc6196217907c2
David Skripnikov <60887632+dsk1020@users.noreply.github.com>
2023-10-22 08:22:43 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+import React from "react";
+import Counter from "./Counter";
+import Name from "./Name";
+// Import the Name component here similar to the Counter above
+
+export default function App() {
+  return (
+    <div className="App">
+      {/* Render the Name component here after importing */}
+      <Counter />
+    </div>
+  );
+}
